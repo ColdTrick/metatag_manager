1f2a844bd7c9fa587bf4718c32584bf3bfc7bc94
Jeroen Dalsem <jdalsem@coldtrick.com>
2013-09-19 09:44:25 +0200

    added dutch translation

---
+++ languages/nl.php
@@ -0,0 +1,11 @@
+<?php
+$language = array (
+  'metatag_manager' => 'Metatag Manager',
+  'metatag_manager:settings:keywords' => 'Keywords',
+  'metatag_manager:settings:description' => 'Omschrijving',
+  'metatag_manager:settings:robots' => 'Robotos',
+  'metatag_manager:settings:author' => 'Auteur',
+  'metatag_manager:settings:custom' => 'Eigen Metatags',
+  'metatag_manager:settings:custom:info' => 'Wees voorzichtig met eigen Metatags!',
+);
+add_translation("nl", $language);
